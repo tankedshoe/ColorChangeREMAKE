be1b7a7208c3eeb3b6739a2658939a08df087dbf
tankedshoe <tankedshoe@gmail.com>
2017-10-12 08:58:14 -0600

    Added a new screen/controller.

---
+++ ColorChangeRemake/Controller/SecondScreenController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondScreenController.swift
+//  ColorChangeRemake
+//
+//  Created by Heaps, Dane on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+class SecondScreenController :UIViewController
+{
+    
+}
